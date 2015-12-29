c3a237bce3fcf1c2a28ed71b3535cd711cb9d936
Italo Moraes <italomoraes@outlook.com>
2015-12-29 18:35:33 -0200

    Add inline edit for demands

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -13,6 +13,16 @@ class DemandsController < ApplicationController
     @demand.destroy
   end
 
+  def edit
+    @retrospective = Retrospective.find(@retrospective_id)
+    @demand = Demand.find(params[:id])
+  end
+
+  def update
+    @demand = Demand.find(params[:id])
+    @demand.update_attributes(demands_params)
+  end
+
   def update_status
     @demand = Demand.find(params[:id])
     @demand.status = !@demand.status
