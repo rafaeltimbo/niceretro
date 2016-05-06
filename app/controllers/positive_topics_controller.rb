6a5b845d4449b8b01bd5caca5e5bb20640fbcd11
Pablo Silva <phsilbr@gmail.com>
2016-05-06 09:20:15 -0300

    Adapt positive topics to work with teams

--- app/controllers/positive_topics_controller.rb
+++ app/controllers/positive_topics_controller.rb
@@ -1,7 +1,6 @@
 class PositiveTopicsController < ApplicationController
   include RemoteFormErrorsHelper
 
-  before_action :retrospective_id
   respond_to :js
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
@@ -9,23 +8,25 @@ class PositiveTopicsController < ApplicationController
   end
 
   def create
-    @retrospective = Retrospective.find(@retrospective_id)
-    @positive_topic = @retrospective.positive_topics.build(positive_topics_params)
+    @retrospective = retrospective
+    @positive_topic = @retrospective.positive_topics.build(positive_topics_params.merge(team: current_team))
     @positive_topic.save
   end
 
   def destroy
-    @positive_topic = PositiveTopic.find(params[:id])
+    @retrospective = retrospective
+    @positive_topic = @retrospective.positive_topics.find(params[:id])
     @positive_topic.destroy
   end
 
   def edit
-    @retrospective = Retrospective.find(@retrospective_id)
-    @positive_topic = PositiveTopic.find(params[:id])
+    @retrospective = retrospective
+    @positive_topic = @retrospective.positive_topics.find(params[:id])
   end
 
   def update
-    @positive_topic = PositiveTopic.find(params[:id])
+    @retrospective = retrospective
+    @positive_topic = @retrospective.positive_topics.find(params[:id])
     @positive_topic.update_attributes(positive_topics_params)
   end
 
@@ -35,7 +36,7 @@ class PositiveTopicsController < ApplicationController
     params.require(:positive_topic).permit(:description)
   end
 
-  def retrospective_id
-    @retrospective_id = params[:retrospective_id]
+  def retrospective
+    current_team.retrospectives.find(params[:retrospective_id])
   end
 end
