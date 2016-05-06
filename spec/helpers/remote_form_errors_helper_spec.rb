ecf4a54bef2f9eaffd6821cfc47391a5296a7491
Pablo Silva <pablo.silva@locaweb.com.br>
2016-05-06 13:40:37 -0300

    Fix broken tests

--- spec/helpers/remote_form_errors_helper_spec.rb
+++ spec/helpers/remote_form_errors_helper_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe RemoteFormErrorsHelper do
 	describe '#errors_for' do
-		
+
 		let(:object) { PositiveTopic.create }
 
 		it 'returns the html formatted error for the invalid attribute' do
 			expect(helper.errors_for(object, :description))
-				.to eq('<small class="ls-help-message">O campo Description não pode ficar em branco</small>')
+				.to eq('<small class="ls-help-message">Este campo não pode ficar em branco</small>')
 		end
 	end
 end
