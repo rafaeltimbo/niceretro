a296dd2c9f426cd5ff8169cca8940874b5fc0626
Rafael Timbó <rafaeltimbosoares@gmail.com>
2018-05-01 14:24:10 -0300

    Http basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_team
 
+  if ENV['HTTP_BASIC_NAME'] && ENV['HTTP_BASIC_PASS']
+    http_basic_authenticate_with(
+      name: ENV['HTTP_BASIC_NAME'],
+      password: ENV['HTTP_BASIC_PASS']
+    )
+  end
+
   def current_team
     Team.find(params[:team_id]) if params[:team_id].present?
   end
